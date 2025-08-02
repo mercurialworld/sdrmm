5e0ec7f1a64740e0ccf9568425943e79272f2d8b
ポッチャマ <mercworld@pocha.moe>
2025-08-02 00:57:24 -0400

    feat: DRM API implementation

---
+++ src/drm.rs
@@ -0,0 +1,122 @@
+use reqwest::Client;
+use serde::de::DeserializeOwned;
+use thiserror::Error;
+use url::Url;
+
+use crate::drm::schema::{DRMHistoryItem, DRMMap, DRMMessage, DRMQueueItem};
+
+pub(crate) mod schema;
+
+#[derive(Debug, Error)]
+pub enum ClientError {
+    #[error("Reqwest error")]
+    ReqwestError(#[from] reqwest::Error),
+    #[error("Serde error")]
+    SerdeError(#[from] serde_json::Error),
+    #[error("URL error")]
+    URLError(#[from] url::ParseError),
+}
+
+type DRMResult<T> = Result<T, ClientError>;
+
+pub struct DRM {
+    client: Client,
+    url: String,
+}
+
+impl DRM {
+    pub fn new(host: String, port: i32) -> Self {
+        let client = Client::new();
+        let url = format!("{}:{}", host, port);
+
+        Self { client, url }
+    }
+
+    pub async fn get_endpoint<T>(&self, endpoint: &str) -> DRMResult<T>
+    where
+        T: DeserializeOwned,
+    {
+        let full_url = Url::parse(&self.url)
+            .map_err(ClientError::URLError)?
+            .join(endpoint);
+
+        let res = self
+            .client
+            .get(full_url?.as_str())
+            .send()
+            .await
+            .map_err(ClientError::ReqwestError)?
+            .text()
+            .await?;
+
+        serde_json::from_str::<T>(&res).map_err(ClientError::SerdeError)
+    }
+
+    // query map info
+    pub async fn query(&self, id: &str) -> DRMResult<DRMMap> {
+        self.get_endpoint(&format!("query/{}", id)).await
+    }
+
+    // the entire queue
+    pub async fn queue(&self) -> DRMResult<DRMMap> {
+        self.get_endpoint("queue").await
+    }
+
+    // all of a user's queue stuff
+    pub async fn queue_where(&self, user: &str) -> DRMResult<Vec<DRMQueueItem>> {
+        self.get_endpoint(&format!("queue/where/{}", user)).await
+    }
+
+    // clear/open/move/shuffle
+    pub async fn queue_control(&self, subcommands: &str) -> DRMResult<DRMMessage> {
+        self.get_endpoint(&format!("queue/{}", subcommands)).await
+    }
+
+    // all history for the session
+    pub async fn history(&self) -> DRMResult<Vec<DRMHistoryItem>> {
+        self.get_endpoint("history").await
+    }
+
+    // most recently played map
+    pub async fn link(&self) -> DRMResult<Vec<DRMHistoryItem>> {
+        self.get_endpoint("history?limit=1").await
+    }
+
+    // add map to queue
+    pub async fn add(&self, id: &str, user: &str) -> DRMResult<DRMMap> {
+        self.get_endpoint(&format!("addKey/{}?user={}", id, user))
+            .await
+    }
+
+    // add map to queue, with optional service
+    pub async fn add_with_service(&self, id: &str, user: &str, service: &str) -> DRMResult<DRMMap> {
+        self.get_endpoint(&format!("addKey/{}?user={}&service={}", id, user, service))
+            .await
+    }
+
+    // add wip to queue
+    pub async fn wip(&self, wip: &str, user: &str) -> DRMResult<DRMMap> {
+        let drm_url = Url::parse(&self.url)
+            .map_err(ClientError::URLError)?
+            .join(&format!("addWip?user={}", user));
+
+        let mut url = wip.into();
+
+        if !wip.starts_with("https://") {
+            // assume it's a wipbot code
+            url = format!("https://wipbot.com/wips/{}.zip", wip);
+        }
+
+        let res = self
+            .client
+            .post(drm_url?.as_str())
+            .body({ url })
+            .send()
+            .await
+            .map_err(ClientError::ReqwestError)?
+            .text()
+            .await?;
+
+        serde_json::from_str::<DRMMap>(&res).map_err(ClientError::SerdeError)
+    }
+}
---
+++ src/drm/schema.rs
@@ -0,0 +1,86 @@
+use chrono::{DateTime, Utc};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct DRMMapDiff {
+    pub difficulty: String,
+    pub characteristic: String,
+    pub note_jump_speed: f32,
+    pub notes_per_second: f32,
+    pub map_mods: MapMods,
+    #[serde(rename = "ScoreSaberStars")]
+    pub scoresaber_stars: f32,
+    #[serde(rename = "BeatLeaderStars")]
+    pub beatleader_stars: f32,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct MapMods {
+    pub chroma: bool,
+    pub cinema: bool,
+    pub mapping_extensions: bool,
+    pub noodle_extensions: bool,
+    pub vivify: bool,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct DRMMap {
+    pub bsr_key: String,
+    pub hash: String,
+    pub user: Option<String>,
+    pub title: String,
+    pub censor_title: bool,
+    pub sub_title: String,
+    pub censor_sub_title: bool,
+    pub artist: String,
+    pub censor_artist: bool,
+    pub mapper: String,
+    pub censor_mapper: bool,
+    #[serde(rename = "MetaDataHasSplicedCensor")]
+    pub metadata_has_spliced_censor: bool,
+    pub duration: i32,
+    pub votes: Vec<i32>,
+    pub rating: f32,
+    pub upload_time: DateTime<Utc>,  // unix epoch
+    pub last_updated: DateTime<Utc>, // unix epoch
+    pub cover: String,
+    pub automapped: bool,
+    #[serde(rename = "ScoreSaberRanked")]
+    pub scoresaber_ranked: f32,
+    #[serde(rename = "BeatLeaderRanked")]
+    pub beatleader_ranked: f32,
+    pub curated: bool,
+    pub curator_name: String,
+    pub playlists: Vec<String>,
+    pub vote_status: i32,
+    pub uses_chroma: bool,
+    pub uses_cinema: bool,
+    pub uses_mapping_extensions: bool,
+    pub uses_noodle_extensions: bool,
+    pub uses_vivify: bool,
+    pub has_played: bool,
+    pub blacklisted: bool,
+    pub diffs: Vec<DRMMapDiff>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct DRMQueueItem {
+    pub spot: i32,
+    pub queue_item: DRMMap,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct DRMHistoryItem {
+    pub timestamp: DateTime<Utc>, // unix epoch
+    pub history_item: DRMMap,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct DRMMessage {
+    pub message: String,
+}
