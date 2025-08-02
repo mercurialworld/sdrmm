fc3a92b4c28bf2f7488896d2bb8e98bc06936bc5
ポッチャマ <mercworld@pocha.moe>
2025-08-02 00:57:40 -0400

    chore: get rust-analyzer to analyze

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod commands;
+#[expect(unused)]
+mod config;
+#[expect(unused)]
+mod database;
+#[expect(unused)]
+mod drm;
+#[expect(unused)]
+mod filter;
+
 fn main() {
     println!("Hello, world!");
 }
